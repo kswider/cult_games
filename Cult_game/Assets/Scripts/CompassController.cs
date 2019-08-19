7c0d3433874b996b2aebda41a7b4a19064cd7f83
sebDoma <sebdomarecki@gmail.com>
2019-08-19 22:31:10 +0200

    Smoothing of compass

--- Cult_game/Assets/Scripts/CompassController.cs
+++ Cult_game/Assets/Scripts/CompassController.cs
@@ -1,9 +1,15 @@
 ﻿using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Android;
+using System.Linq;
 
 public class CompassController : MonoBehaviour
 {
+    public int smoothingProbes = 15;
+    
+    private List<float> _headings;
+    
     public void Awake()
     {
         StartCoroutine(InitializeLocation());
@@ -59,11 +65,18 @@ public class CompassController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        _headings = new List<float>();
         InvokeRepeating(nameof(UpdateCompass), 0.5f, 0.025f);
     }
-    
-    void UpdateCompass()
+
+    private void UpdateCompass()
     {
-        transform.localRotation = Quaternion.Euler(0, 0, Mathf.Round(Input.compass.trueHeading));
+        if (_headings.Count > smoothingProbes)
+        {
+            _headings.RemoveAt(0);
+        }
+        _headings.Add(Input.compass.trueHeading);
+        
+        transform.localRotation = Quaternion.Euler(0, 0, _headings.Average());
     }
 }
