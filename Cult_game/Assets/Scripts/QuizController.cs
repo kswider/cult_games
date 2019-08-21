89aac0179817dc1387128e69f37fadf48ad2ded1
Krzysiek <krzyztof.swider@motorolasolutions.com>
2019-08-21 21:15:23 +0200

    Created initial scene for quiz

---
+++ Cult_game/Assets/Scripts/QuizController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuizController : MonoBehaviour
+{
+    QuizParametersHolder parameters;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        parameters = GameObject.FindObjectOfType<QuizParametersHolder>();
+        Debug.LogError($"Passed parameter = {parameters?.QuizNumber ?? 100}");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Cult_game/Assets/Scripts/QuizParametersHolder.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuizParametersHolder : MonoBehaviour
+{
+
+    public int QuizNumber = 5;
+    // Start is called before the first frame update
+    void Start()
+    {
+        DontDestroyOnLoad(this);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
