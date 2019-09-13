c4053b94b1fc362a396c2c6d9f6eee0de667e277
Krzysiek <krzyztof.swider@motorolasolutions.com>
2019-09-13 23:24:21 +0200

    Added initial version of server

---
+++ Cult_games_server/Data/DataContext.cs
@@ -0,0 +1,12 @@
+﻿using Cult_games_server.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Cult_games_server.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+
+        public DbSet<Player> Players { get; set; }
+    }
+}
---
+++ Cult_games_server/Data/IGameRepository.cs
@@ -0,0 +1,17 @@
+﻿using Cult_games_server.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Cult_games_server.Data
+{
+    public interface IGameRepository
+    {
+        void Add<T>(T entity) where T : class;
+        void Delete<T>(T entity) where T : class;
+        Task<Player> GetPlayer(string name);
+        Task<IEnumerable<Player>> GetPlayers();
+        Task<bool> SaveAll();
+    }
+}
